7a8e99e9988aa5f4fa33ff479674254bf9cb44e1
Ken Finnigan <ken.finnigan@workday.com>
2021-12-03 16:58:17 -0500

    Day 3 part one and partial for part two

---
+++ 2021/03/part-two/src/main.rs
@@ -0,0 +1,94 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn main() {
+    let file = File::open("../input.txt").unwrap();
+    let input: Vec<String> = BufReader::new(file)
+        .lines()
+        .collect::<Result<_, _>>().unwrap();
+
+    let result = life_support(&input, 12);
+
+    println!("Part 2: {}", result);
+}
+
+fn life_support(data: &Vec<String>, size: i32) -> i32 {
+
+    for line in data {
+        let chars: Vec<char> = line.chars().collect();
+
+        let mut count = 0;
+        for c in chars {
+            counters[count] += c.to_digit(10).unwrap() as i32;
+            count += 1;
+        }
+        records += 1;
+    }
+
+    let mut gamma: String = String::new();
+    let mut epsilon: String = String::new();
+    for count in counters {
+        if count > records / 2 {
+            gamma += "1";
+            epsilon += "0";
+        } else {
+            gamma += "0";
+            epsilon += "1";
+        }
+    }
+
+    i32::from_str_radix(&gamma, 2).unwrap() * i32::from_str_radix(&epsilon, 2).unwrap()
+}
+
+fn highest_usage(data: &Vec<String>, size: i32, bit_pos: i32) -> String {
+    let mut counters: Vec<i32> = vec![0; size.try_into().unwrap()];
+    let mut records = 0;
+
+    for line in data {
+        let chars: Vec<char> = line.chars().collect();
+
+        let mut count = 0;
+        for c in chars {
+            counters[count] += c.to_digit(10).unwrap() as i32;
+            count += 1;
+        }
+        records += 1;
+    }
+
+    let mut gamma: String = String::new();
+    for count in counters {
+        if count > records / 2 {
+            gamma += "1";
+        } else {
+            gamma += "0";
+        }
+    }
+
+    gamma
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let input: Vec<String> = "00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010".split("\n")
+        .map(|s| s.trim().to_string())
+        .collect();
+
+        assert_eq!(power(&life_support, 5), 230);
+    }
+}
